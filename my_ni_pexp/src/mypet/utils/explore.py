2d9e0faf862a71d6e624f7e31814cf8b5c697b0d
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2013-05-27 08:56:32 +0200

    forgot utils

---
+++ my_ni_pexp/src/mypet/utils/explore.py
@@ -0,0 +1,94 @@
+'''
+Created on 24.05.2013
+
+@author: robert
+'''
+import itertools as itools
+
+
+
+def cartesian_product(param_value_list_dict, combined_parameter_list=[]):
+    
+    if not combined_parameter_list:
+        combined_parameter_list = list(param_value_list_dict)
+    
+    for key in param_value_list_dict:
+        inlist = False
+        for ptuple in combined_parameter_list:
+            if key in ptuple:
+                inlist = True
+                break;
+        
+        if not inlist:
+            combined_parameter_list.append((key,))
+                    
+    
+    idx_lists = []
+    for idx, paramtuple in enumerate(combined_parameter_list):
+        if not isinstance(paramtuple, tuple):
+            combined_parameter_list[idx] = (combined_parameter_list[idx],)
+            paramtuple = combined_parameter_list[idx]
+            
+        for idx, param in enumerate(paramtuple):
+            value_list = param_value_list_dict[param]
+            if not isinstance(value_list, list):
+                raise TypeError(param + ' is not a list, why should I explore it anyway?')
+            
+            if idx == 0:
+                param_length = len(value_list)
+                idx_lists.append(range(param_length))
+            else:
+                if not param_length == len(value_list):
+                    raise TypeError('Lists of two combined parameters are unequal!')
+
+    
+    product_lists = list(itools.product(*idx_lists))
+    zipped_lists = zip(*product_lists)
+    
+    result_dict={}
+    result_combined_list=[[]]
+    for idx, ituple in enumerate(zipped_lists):
+        for param in combined_parameter_list[idx]:
+            result_dict[param] = []
+            result_combined_list[0].append(param)
+            for tupidx in ituple:
+                result_dict[param].append(param_value_list_dict[param][tupidx])
+    
+    result_combined_list[0]=tuple(result_combined_list[0])
+    return result_dict, result_combined_list
+            
+                
+        
+        
+    
+
+# def _do_cartesian_product( value_lists):
+# 
+#     assert isinstance(value_lists, list)
+#     
+#     tuple_list =  list(itools.product(*value_lists))
+#     
+#     
+#     zipped_list = zip(*tuple_list)
+#     
+#     result_lists = []
+#     for ptuple in zipped_list:
+#         result_lists.append(list(ptuple))
+#     
+#     return result_lists
+
+# def _to_dictionary(param_name_list, value_lists):
+#     
+#     result_dict={}
+#     for idx, param in enumerate(param_name_list):
+#     
+#         name_data = param.split(".")
+#         param_name = ".".join(name_data[:-1])
+#         value_str = name_data[-1]
+#         
+#         if not param_name in result_dict:
+#             result_dict[param_name] = {}
+#         
+#         result_dict[param_name][value_str]= value_lists[idx]
+#     
+#     return result_dict
---
+++ my_ni_pexp/src/test_explore.py
@@ -0,0 +1,47 @@
+'''
+Created on 20.05.2013
+
+@author: robert
+'''
+from mypet.parameter import *
+from mypet.trajectory import *
+import numpy as np
+import scipy.sparse as spsp
+import mypet.utils.explore as ut
+
+
+
+def main():
+    
+    logging.basicConfig(level=logging.DEBUG)
+    traj = Trajectory('MyTrajectory','../experiments/test.hdf5')
+    
+    #traj.load_trajectory(trajectoryname='MyTrajectory_2013_05_23_14h29m26s')
+    
+    traj.add_parameter('test.testparam', {'Fuechse':1,'Sapiens':1,'Comment':'ladida'}, param_type=Parameter)
+
+
+    traj.add_parameter(full_parameter_name='honky', value_dict={'mirta':np.array([[1,2,7],[3,2,17]])}, param_type=Parameter)
+
+    traj.add_parameter('flonky',{'val' : 10})
+    
+    param1 = traj.Parameters.test.testparam
+    param2 = traj.Parameters.honky
+    param3 = traj.last
+    
+    exp2_list = range(30)
+    exp1_list = range(30)
+    exp3_list = range(30)
+    explore_dict = { param1('Sapiens') : exp1_list,
+                     param2.get_fullname('mirta'):exp2_list,
+                     param3('val') : exp3_list}
+    
+    cmb_list=[(param3('val'),param1('Sapiens'))]
+    
+    traj.explore(ut.cartesian_product,explore_dict,cmb_list)
+
+    traj.store_to_hdf5()
+    
+
+if __name__ == '__main__':
+    main()
