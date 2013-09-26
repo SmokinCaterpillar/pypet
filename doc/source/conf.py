fa95f3dfbd300fc79d3c60d8cfc2fd4990c13c92
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2013-09-26 11:10:04 +0200

    mocking modules for rdt

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -262,6 +262,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['tables', 'numpy', 'brian' ,'pandas']
+MOCK_MODULES = ['tables', 'numpy', 'brian' ,'pandas', 'brian.units']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
