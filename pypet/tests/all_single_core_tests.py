b4c8b0d856ecbf7f11a3b76bf0be1762599f774c
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2014-03-28 14:44:10 +0100

    fixed yaml including dill, fixed test suite

--- pypet/tests/all_single_core_tests.py
+++ pypet/tests/all_single_core_tests.py
@@ -10,7 +10,7 @@ from pypet.tests.parameter_test import ArrayParameterTest,PickleParameterTest,Sp
 from pypet.tests.trajectory_test import SingleRunQueueTest, SingleRunTest, TrajectoryFindTest, TrajectoryMergeTest, TrajectoryTest
 from pypet.tests.hdf5_storage_test import ResultSortTest, EnvironmentTest, StorageTest, TestOtherHDF5Settings, TestOtherHDF5Settings2
 from pypet.tests.hdf5_merge_test import TestMergeResultsSort, MergeTest
-from pypet.tests.hdf5_removal_and_continue_tests import ContinueTest
+
 from pypet.tests.utilstest import CartesianTest
 from pypet.tests.environment_test import EnvironmentTest
 from pypet.tests.annotations_test import AnnotationsTest
@@ -23,6 +23,12 @@ try:
     if not os.getenv('TRAVIS',False):
         from pypet.tests.briantests.brian_monitor_test import BrianMonitorTest
         from pypet.tests.briantests.brian_full_network_test import NetworkTest, NetworkMPTest
+except ImportError as e:
+    print repr(e)
+    pass
+
+try:
+    from pypet.tests.hdf5_removal_and_continue_tests import ContinueTest
 except ImportError as e:
     print repr(e)
     pass
