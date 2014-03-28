b0d5d1de2513ec25c3a8b2f26e0eb31ef4338442
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2014-03-28 17:13:20 +0100

    Revert "fixed yaml including dill, fixed test suite"
    This reverts commit b4c8b0d856ecbf7f11a3b76bf0be1762599f774c.

--- pypet/tests/all_tests.py
+++ pypet/tests/all_tests.py
@@ -7,6 +7,16 @@ import os
 from pypet.tests.all_single_core_tests import *
 from pypet.tests.all_multi_core_tests import *
 
+# Works only if someone has installed Brian
+try:
+    from pypet.tests.briantests.brian_parameter_test import BrianParameterTest, BrianParameterStringModeTest, \
+        BrianResult, BrianResultStringModeTest
+    if not os.getenv('TRAVIS',False):
+        from pypet.tests.briantests.brian_monitor_test import BrianMonitorTest
+        from pypet.tests.briantests.brian_full_network_test import NetworkTest, NetworkMPTest
+except ImportError as e:
+    print repr(e)
+    pass
 
 
 if __name__ == '__main__':
