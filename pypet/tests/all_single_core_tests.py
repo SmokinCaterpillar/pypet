41c29b92ca575188e6225cfb60d61affa3ae9d90
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2014-03-31 11:54:10 +0200

    Fixed travis file, now including dill

--- pypet/tests/all_single_core_tests.py
+++ pypet/tests/all_single_core_tests.py
@@ -10,7 +10,11 @@ from pypet.tests.parameter_test import ArrayParameterTest,PickleParameterTest,Sp
 from pypet.tests.trajectory_test import SingleRunQueueTest, SingleRunTest, TrajectoryFindTest, TrajectoryMergeTest, TrajectoryTest
 from pypet.tests.hdf5_storage_test import ResultSortTest, EnvironmentTest, StorageTest, TestOtherHDF5Settings, TestOtherHDF5Settings2
 from pypet.tests.hdf5_merge_test import TestMergeResultsSort, MergeTest
-from pypet.tests.hdf5_removal_and_continue_tests import ContinueTest
+try:
+    from pypet.tests.hdf5_removal_and_continue_tests import ContinueTest
+except ImportError as e:
+    print repr(e) # We end up here if `dill` is not installed
+    pass
 from pypet.tests.utilstest import CartesianTest
 from pypet.tests.environment_test import EnvironmentTest
 from pypet.tests.annotations_test import AnnotationsTest
@@ -24,7 +28,7 @@ try:
         from pypet.tests.briantests.brian_monitor_test import BrianMonitorTest
         from pypet.tests.briantests.brian_full_network_test import NetworkTest, NetworkMPTest
 except ImportError as e:
-    print repr(e)
+    print repr(e) # We end up here if `brian` is not installed
     pass
 
 
