05e5516c971a426edbe87f45d6c9fd36020417ac
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2014-07-30 14:24:36 +0200

    proper import of tests

--- pypet/tests/all_multi_core_tests.py
+++ pypet/tests/all_multi_core_tests.py
@@ -19,11 +19,7 @@ from pypet.tests.test_helpers import make_run
 
 # Works only if someone has installed Brian
 try:
-    from pypet.tests.briantests.brian_parameter_test import BrianParameterTest, BrianParameterStringModeTest, \
-        BrianResult, BrianResultStringModeTest
-    # if not os.getenv('TRAVIS',False):
-    from pypet.tests.briantests.brian_monitor_test import BrianMonitorTest
-    from pypet.tests.briantests.brian_full_network_test import BrianFullNetworkTest, BrianFullNetworkMPTest
+    from pypet.tests.briantests.brian_full_network_test import  BrianFullNetworkMPTest
 except ImportError as e:
     print(repr(e))
     pass
