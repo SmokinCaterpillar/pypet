b952711acf760630ecb3cd1103734200dd070630
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2014-08-20 14:59:05 +0200

    backwards compat test only for python 2

--- pypet/tests/backwards_compat_test.py
+++ pypet/tests/backwards_compat_test.py
@@ -14,13 +14,17 @@ import pypet
 class LoadOldTrajectoryTest(unittest.TestCase):
 
     def test_backwards_compatibility(self):
-        old_pypet_traj = Trajectory()
-        module_path, init_file = os.path.split(pypet.__file__)
-        filename= os.path.join(module_path, 'tests','testdata','pypet_v0_1b_6.hdf5')
-        old_pypet_traj.f_load(filename=filename,
-                              load_all=2, force=True, index=-1)
+        if (sys.version_info < (3, 0, 0)):
+            # Test only makes sense with python 2.7 or lower
+            old_pypet_traj = Trajectory()
+            module_path, init_file = os.path.split(pypet.__file__)
+            filename= os.path.join(module_path, 'tests','testdata','pypet_v0_1b_6.hdf5')
+            old_pypet_traj.f_load(filename=filename,
+                                  load_all=2, force=True, index=-1)
 
-        self.assertTrue(old_pypet_traj.v_version=='0.1b.6')
-        self.assertTrue(old_pypet_traj.par.x==0)
-        self.assertTrue(len(old_pypet_traj)==9)
-        self.assertTrue(old_pypet_traj.res.runs.r_4.z==12)
+            self.assertTrue(old_pypet_traj.v_version=='0.1b.6')
+            self.assertTrue(old_pypet_traj.par.x==0)
+            self.assertTrue(len(old_pypet_traj)==9)
+            self.assertTrue(old_pypet_traj.res.runs.r_4.z==12)
+        else:
+            pass
