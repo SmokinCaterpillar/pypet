9411d66c264d10f5bd01a24485c1dd2b58ae7263
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2014-10-09 12:00:49 +0200

    Fast access is true by default

--- pypet/naturalnaming.py
+++ pypet/naturalnaming.py
@@ -2394,7 +2394,7 @@ class NNGroupNode(NNTreeNode):
         """
         return self._nn_interface._get_all(self, name, max_depth=max_depth)
 
-    def f_get_default(self, name, default=None, fast_access=False, backwards_search=False,
+    def f_get_default(self, name, default=None, fast_access=True, backwards_search=False,
               shortcuts=True, max_depth=None, auto_load=False):
         """ Similar to `f_get`, but returns the default value if `name` is not found in the
         trajectory.
@@ -2402,6 +2402,8 @@ class NNGroupNode(NNTreeNode):
         This function uses the `f_get` method and will return the default value
         in case `f_get` raises an AttributeError or a DataNotInStorageError.
         Other errors are not handled.
+
+        In contrast to `f_get`, fast access is True by default.
 
 
         """
