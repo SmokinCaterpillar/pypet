d9c1447281edeb4aba4e3d1e62ad55af6a474a54
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-01-10 11:07:30 +0100

    fixed python 3 print statements in simple tests

--- pypet/tests/simpletests/simple_env_test.py
+++ pypet/tests/simpletests/simple_env_test.py
@@ -70,7 +70,7 @@ traj.f_load(index=-1, load_parameters=2, load_results=2)
 
 # Finally we want to print a result of a particular run.
 # Let's take the second run named `run_00000001` (Note that counting starts at 0!).
-print 'The result of `run_00000001` is: '
-print traj.run_00000001.z
+print('The result of `run_00000001` is: ')
+print(traj.run_00000001.z)
 
 remove_data()
--- pypet/tests/simpletests/simple_mp_env_test.py
+++ pypet/tests/simpletests/simple_mp_env_test.py
@@ -75,8 +75,8 @@ def main():
 
     # Finally we want to print a result of a particular run.
     # Let's take the second run named `run_00000001` (Note that counting starts at 0!).
-    print 'The result of `run_00000001` is: '
-    print traj.run_00000001.z
+    print('The result of `run_00000001` is: ')
+    print(traj.run_00000001.z)
 
     remove_data()
 
