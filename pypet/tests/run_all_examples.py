d5cdc81036ce01420f0c911a22e3980fb8a8b649
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-01-11 03:21:23 +0100

    skipping multiprocessing examples under windows because they don't work with execfile

--- pypet/tests/run_all_examples.py
+++ pypet/tests/run_all_examples.py
@@ -10,17 +10,29 @@ matplotlib.use('Agg')
 import glob
 import os
 import sys
+import platform
 try:
     import brian
 except ImportError:
     print('No BRIAN module found, will skip the example')
     brian = None
 
+system = platform.system()
+
+if system == 'Windows':
+    skiplist = ['04', '07', '11', '13']
+else:
+    skiplist = []
+
+def skip(name):
+    for item in skiplist:
+        if item in name:
+            return True
+    return False
 
 os.chdir(os.path.join('..','..','examples'))
 sys.path.append(os.getcwd())
 simple_examples = glob.glob('*.py')
-
 assert len(simple_examples) == 11 + 1
 
 for simple_example in simple_examples:
@@ -28,7 +40,7 @@ for simple_example in simple_examples:
         continue
 
     filename = os.path.join(os.getcwd(), simple_example)
-    if 'brian' in simple_example and brian is None:
+    if ('brian' in simple_example and brian is None) or skip(simple_example):
         print("---------- Skipping %s ----------" % simple_example)
     else:
         print("########## Running %s ###########" % simple_example)
@@ -39,27 +51,32 @@ for simple_example in simple_examples:
 
 
 ex13 = 'example_13_post_processing'
-print("########## Running %s ###########" % ex13)
-os.chdir(ex13)
-sys.path.append(os.getcwd())
-print("Running main")
-filename = os.path.join(os.getcwd(), 'main.py')
-with open(filename) as f:
-    code = compile(f.read(), filename, 'exec')
-    exec(code, globals(), locals())
-print("Running analysis")
-filename = os.path.join(os.getcwd(), 'analysis.py')
-with open(filename) as f:
-    code = compile(f.read(), filename, 'exec')
-    exec(code, globals(), locals())
-print("Running pipeline")
-filename = os.path.join(os.getcwd(), 'pipeline.py')
-with open(filename) as f:
-    code = compile(f.read(), filename, 'exec')
-    exec(code, globals(), locals())
+if skip(ex13):
+    print("------- Skipping %s -------" % ex13)
+else:
+    print("########## Running %s ###########" % ex13)
+    os.chdir(ex13)
+    sys.path.append(os.getcwd())
+    print("Running main")
+    filename = os.path.join(os.getcwd(), 'main.py')
+    with open(filename) as f:
+        code = compile(f.read(), filename, 'exec')
+        exec(code, globals(), locals())
+    print("Running analysis")
+    filename = os.path.join(os.getcwd(), 'analysis.py')
+    with open(filename) as f:
+        code = compile(f.read(), filename, 'exec')
+        exec(code, globals(), locals())
+    print("Running pipeline")
+    filename = os.path.join(os.getcwd(), 'pipeline.py')
+    with open(filename) as f:
+        code = compile(f.read(), filename, 'exec')
+        exec(code, globals(), locals())
 
 ex11 = 'example_11_large_scale_brian_simulation'
-if brian is not None:
+if brian is None or skip(ex11):
+    print("------- Skipping %s -------" % ex11)
+else:
     print("########## Running %s ###########" % ex11)
     os.chdir('..')
     os.chdir(ex11)
@@ -74,5 +91,3 @@ if brian is not None:
     with open(filename) as f:
         code = compile(f.read(), filename, 'exec')
         exec(code, globals(), locals())
-else:
-    print("------- Skipping %s -------" % ex11)
