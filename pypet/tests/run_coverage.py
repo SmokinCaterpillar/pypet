5e2f316cdddb6bf6e51f17861400ccd062b2c74b
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-01-12 16:12:04 +0100

    new coverage script

---
+++ pypet/tests/run_coverage.py
@@ -0,0 +1,28 @@
+__author__ = 'Robert Meyer'
+
+import getopt
+import sys
+import os
+
+from pypet.tests.all_single_core_tests import *
+
+from pypet.tests.all_multi_core_tests import TestMPImmediatePostProc,\
+    MultiprocLinkNoPoolLockTest, MultiprocLinkNoPoolQueueTest, MultiprocLinkLockTest,\
+    MultiprocLinkQueueTest
+
+
+if __name__ == '__main__':
+    opt_list, _ = getopt.getopt(sys.argv[1:],'k',['folder='])
+    remove = None
+    folder = None
+    for opt, arg in opt_list:
+        if opt == '-k':
+            remove = False
+            print('I will keep all files.')
+
+        if opt == '--folder':
+            folder = arg
+            print('I will put all data into folder `%s`.' % folder)
+
+    sys.argv=[sys.argv[0]]
+    make_run(remove, folder)
