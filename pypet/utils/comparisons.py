86c6fafc83cf20737972492af84866a70cc81d15
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-04-06 17:03:29 +0200

    Refactored nested equality comparison.

--- pypet/utils/comparisons.py
+++ pypet/utils/comparisons.py
@@ -130,7 +130,17 @@ def get_all_attributes(instance):
 
 
 def nested_equal(a, b):
-    """Compares two objects recursively by their elements, also handling numpy objects.
+    """Compares two objects recursively by their elements.
+
+    Also handles numpy arrays, pandas data and sparse matrices.
+
+    First checks if the data falls into the above categories.
+    If not, it is checked if a or b are some type of sequence or mapping and
+    the contained elements are compared.
+    If this is not the case, it is checked if a or b do provide a custom `__eq__` that
+    evaluates to a single boolean value.
+    If this is not the case, the attributes of a and b are compared.
+    If this does not help either, normal `==` is used.
 
     Assumes hashable items are not mutable in a way that affects equality.
     Based on the suggestion from HERE_, thanks again Lauritz V. Thaulow :-)
@@ -144,92 +154,100 @@ def nested_equal(a, b):
     if a is None or b is None:
         return False
 
-    if isinstance(a, pypetconstants.PARAMETER_SUPPORTED_DATA):
-        if not isinstance(b, pypetconstants.PARAMETER_SUPPORTED_DATA):
-            return False
-        return a == b
-
-    if spsp.isspmatrix(a) and spsp.isspmatrix(b):
+    a_sparse = spsp.isspmatrix(a)
+    b_sparse = spsp.isspmatrix(b)
+    if a_sparse != b_sparse:
+        return False
+    if a_sparse:
         if a.nnz == 0:
             return b.nnz == 0
         else:
             return not np.any((a != b).data)
 
-    if isinstance(a, (pd.Panel, pd.Panel4D)):
-        if not isinstance(b, (pd.Panel, pd.Panel4D)):
-            return False
+    a_panel = isinstance(a, (pd.Panel, pd.Panel4D))
+    b_panel = isinstance(b, (pd.Panel, pd.Panel4D))
+    if a_panel != b_panel:
+        return False
+    if a_panel:
         return nested_equal(a.to_frame(), b.to_frame())
 
-    if isinstance(a, (pd.DataFrame, pd.Series)):
-        try:
-            if type(a) is not type(b):
+    a_series = isinstance(a, pd.Series)
+    b_series = isinstance(b, pd.Series)
+    if a_series != b_series:
+        return False
+    if a_series:
+        eq = (a == b).all()
+        if isinstance(eq, (bool, np.bool_)):
+            return eq
+        else:
+            if not len(a) == len(b):
                 return False
-            if isinstance(a, pd.DataFrame) and a.empty and b.empty:
+            for idx, itema in enumerate(a):
+                itemb = b[idx]
+                if not nested_equal(itema, itemb):
+                    return False
+            return True
+
+    a_frame = isinstance(a, pd.DataFrame)
+    b_frame = isinstance(b, pd.DataFrame)
+    if a_frame != b_frame:
+        return False
+    if a_frame:
+        try:
+            if a.empty and b.empty:
                 return True
             new_frame = a == b
             new_frame = new_frame | (pd.isnull(a) & pd.isnull(b))
             if isinstance(new_frame, pd.DataFrame):
                 return np.all(new_frame.as_matrix())
-            else:
-                eq = new_frame.all() # In older pandas versions,
-                # series do not support as_matrix
-                if isinstance(eq, (bool, np.bool_)):
-                    return eq
-                else:
-                    raise ValueError('No boolean value')
         except (ValueError, TypeError):
             # The Value Error can happen if the data frame is of dtype=object and contains
             # numpy arrays. Numpy array comparisons do not evaluate to a single truth value
-            if isinstance(a, pd.DataFrame):
-                for name in a:
-                    cola = a[name]
-                    if not name in b:
-                        return False
-                    colb = b[name]
-                    if not len(cola) == len(colb):
-                        return False
-                    for idx, itema in enumerate(cola):
-                        itemb = colb[idx]
-                        if not nested_equal(itema, itemb):
-                            return False
-            else:
-                if not len(a) == len(b):
+            for name in a:
+                cola = a[name]
+                if not name in b:
                     return False
-                for idx, itema in enumerate(a):
-                    itemb = b[idx]
+                colb = b[name]
+                if not len(cola) == len(colb):
+                    return False
+                for idx, itema in enumerate(cola):
+                    itemb = colb[idx]
                     if not nested_equal(itema, itemb):
                         return False
             return True
 
-    if isinstance(a, np.ndarray):
-        if not isinstance(b, np.ndarray):
-            return False
+    a_array = isinstance(a, np.ndarray)
+    b_array = isinstance(b, np.ndarray)
+    if a_array != b_array:
+        return False
+    if a_array:
         if a.shape != b.shape:
             return False
         return np.all(a == b)
 
-    if isinstance(a, Sequence):
-        if not isinstance(b, Sequence):
-            return False
+    a_list = isinstance(a, (Sequence, list, tuple))
+    b_list = isinstance(b, (Sequence, list, tuple))
+    if a_list != b_list:
+        return False
+    if a_list:
         return all(nested_equal(x, y) for x, y in zip(a, b))
 
-    if isinstance(a, Mapping):
-        if not isinstance(b, Mapping):
-            return False
+    a_mapping = isinstance(a, (Mapping, dict))
+    b_mapping = isinstance(b, (Mapping, dict))
+    if a_mapping != b_mapping:
+        return False
+    if a_mapping:
         keys_a = a.keys()
         if set(keys_a) != set(b.keys()):
             return False
         return all(nested_equal(a[k], b[k]) for k in keys_a)
 
+    # Equality for general objects
     # for types that support __eq__
     try:
-        custom_eq = a.__eq__(b)  # Best way I came up with to check in python 2 and 3
-        # if equality is implemented
-        if isinstance(custom_eq, (bool, np.bool_)):
-            return custom_eq
-        all_equals = all(custom_eq)
-        if isinstance(all_equals, (bool, np.bool_)):
-            return all_equals
+        equality = a.__eq__(b)
+        if isinstance(equality, (bool, np.bool_)):
+            return equality
     except (AttributeError, NotImplementedError, TypeError, ValueError):
         pass
 
@@ -244,4 +262,9 @@ def nested_equal(a, b):
 
         return all(nested_equal(attributes_a[k], attributes_b[k]) for k in keys_a)
 
-    return False
+    # Finally let's go for simple equality if the rest does not work
+    equality = a == b
+    if isinstance(equality, (bool, np.bool_)):
+        return equality
+    else:
+        return False
