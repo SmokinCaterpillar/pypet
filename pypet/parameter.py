2fb63d4a18257ada2d74a34bfcaa004b7da5f55c
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-04-07 13:40:47 +0200

    fool around

--- pypet/parameter.py
+++ pypet/parameter.py
@@ -981,7 +981,7 @@ class Parameter(BaseParameter):
         val = self._convert_data(data)
 
         if not self.f_supports(val):
-            raise TypeError('Unsupported data `%s` of tpye `%s`' % (str(val), str(type(val))))
+            raise TypeError('Unsupported data `%s` of type `%s`' % (str(val), str(type(val))))
 
         self._data = val
         self._default = self._data
