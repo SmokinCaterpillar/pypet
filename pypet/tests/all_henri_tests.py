1b41ebdbe45bfcb40d8be6a317ac7cdbf9c3f268
henribunting <info@henrib.info>
2015-06-04 13:32:03 +0200

    Fifth travis attempt, printing suite

--- pypet/tests/all_henri_tests.py
+++ pypet/tests/all_henri_tests.py
@@ -7,4 +7,5 @@ from pypet.tests.testutils.ioutils import run_suite, discover_tests, TEST_IMPORT
 if __name__ == '__main__':
     opt_dict = parse_args()
     suite = discover_tests(predicate=lambda class_name, test_name, tags: 'brian2' in tags)
+    print(suite)
     run_suite(suite=suite, **opt_dict)
