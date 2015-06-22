e08f13cd4794cc7f9a1429e8428fd8d424f2395a
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-06-22 20:51:27 +0200

    Testing sumatra with newer django version;

--- ciscripts/travis/install_gitpython.sh
+++ ciscripts/travis/install_gitpython.sh
@@ -6,6 +6,6 @@ pip install GitPython==0.3.6 # Sumatra has a wrong version checking
 # which thinks GitPython 1.0.0 is lower than 0.3.6 (only checking minor version :(
 if [[ $TRAVIS_PYTHON_VERSION == 2* ]]
     then
-        pip install django==1.5
+        pip install django #==1.5 maybe this works now with newer django versions
         pip install Sumatra
     fi
