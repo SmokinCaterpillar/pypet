7e5ba301b569fab21a2017c72c295477af42601d
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-08-17 11:45:14 +0200

    Added FrozenScoop tets to coverage;

--- pypet/tests/coverage_run.py
+++ pypet/tests/coverage_run.py
@@ -53,6 +53,7 @@ if __name__ == '__main__':
                     'MultiprocLinkQueueTest',
                     'MultiprocPoolSortLocalTest',
                     'MultiprocSCOOPSortLocalTest',
+                    'MultiprocFrozenSCOOPSortLocalTest',
                     'CapTest'))
     pred = lambda class_name, test_name, tags: (class_name in tests_include or
                                                  'multiproc' not in tags)
