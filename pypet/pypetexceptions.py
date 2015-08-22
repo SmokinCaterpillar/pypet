2fd7eb428023146713912376837a25d627ccbaac
Robert Meyer <robert.meyer@ni.tu-berlin.de>
2015-08-22 13:04:22 +0200

    ENH: Removed unnecessary exception code;

--- pypet/pypetexceptions.py
+++ pypet/pypetexceptions.py
@@ -5,24 +5,13 @@ __author__ = 'Robert Meyer'
 
 class ParameterLockedException(TypeError):
     """Exception raised if someone tries to modify a locked Parameter."""
-
-    def __init__(self, msg):
-        self._msg = msg
-
-    def __str__(self):
-        return repr(self._msg)
+    pass
 
 
 class VersionMismatchError(TypeError):
     """Exception raised if the current version of pypet does not match the version with which
         the trajectory was handled."""
-
-    def __init__(self, msg):
-        self._msg = msg
-
-    def __str__(self):
-        return repr(self._msg)
-
+    pass
 
 class PresettingError(Exception):
     """Exception raised if parameter presetting failed.
@@ -30,12 +19,7 @@ class PresettingError(Exception):
     Probable cause might be a typo in the parameter name.
 
     """
-
-    def __init__(self, msg):
-        self._msg = msg
-
-    def __str__(self):
-        return repr(self._msg)
+    pass
 
 
 class NoSuchServiceError(TypeError):
@@ -43,51 +27,26 @@ class NoSuchServiceError(TypeError):
     i.e. the message is not understood.
 
     """
-
-    def __init__(self, msg):
-        self._msg = msg
-
-    def __str__(self):
-        return repr(self._msg)
+    pass
 
 
 class NotUniqueNodeError(AttributeError):
     """Exception raised by the Natural Naming if a node can be found more than once."""
-
-    def __init__(self, msg):
-        self._msg = msg
-
-    def __str__(self):
-        return repr(self._msg)
+    pass
 
 
 class TooManyGroupsError(TypeError):
     """Exception raised by natural naming fast search if fast search cannot be applied.
     """
-
-    def __init__(self, msg):
-        self._msg = msg
-
-    def __str__(self):
-        return repr(self._msg)
+    pass
 
 
 class DataNotInStorageError(IOError):
     """Excpetion raise by Storage Service if data that is supposed to be loaded cannot
     be found on disk."""
-
-    def __init__(self, msg):
-        self._msg = msg
-
-    def __str__(self):
-        return repr(self._msg)
+    pass
 
 
 class GitDiffError(RuntimeError):
     """Exception raised if there are uncommited changes."""
-
-    def __init__(self, msg):
-        self._msg = msg
-
-    def __str__(self):
-        return repr(self._msg)
+    pass
