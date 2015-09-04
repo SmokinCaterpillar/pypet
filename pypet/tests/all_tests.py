44a1d0a353c169f6464354f76f9ef94f0095c8cd
Mehmet Nevvaf Timur <nvvftimur92@gmail.com>
2015-09-04 15:16:33 +0200

    TEST: Reverted all test starts back to normal to allow merging;

--- pypet/tests/all_tests.py
+++ pypet/tests/all_tests.py
@@ -7,5 +7,5 @@ from pypet.tests.testutils.ioutils import run_suite, discover_tests, TEST_IMPORT
 if __name__ == '__main__':
     opt_dict = parse_args()
     suite = discover_tests(predicate=lambda class_name, test_name, tags:
-                                                class_name != TEST_IMPORT_ERROR and 'shared' in tags)
+                                                class_name != TEST_IMPORT_ERROR)
     run_suite(suite=suite, **opt_dict)
