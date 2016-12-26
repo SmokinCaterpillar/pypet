0f4d202f29668e0034e608b7d3a16d4069f16cf7
Robert Meyer <robert.meyer@flixbus.com>
2016-12-26 11:34:43 +0100

    Fixed all getChild method from old pytables versions;

--- pypet/tests/unittests/storage_test.py
+++ pypet/tests/unittests/storage_test.py
@@ -1053,7 +1053,7 @@ class StorageTest(TrajectoryComparator):
 
             hdf5file = pt.open_file(filename=filename)
 
-            table= hdf5file.root._f_getChild(traj.v_name)._f_getChild('overview')._f_getChild('hdf5_settings')
+            table= hdf5file.root._f_get_child(traj.v_name)._f_get_child('overview')._f_get_child('hdf5_settings')
 
             row = table[0]
 
