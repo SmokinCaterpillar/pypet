5a3f4cc6660868062cff8b14bcfd7e7c247f3a61
Robert Meyer <robert.meyer@flixbus.com>
2017-10-23 15:08:14 +0200

    towards 0.4.2dev

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     install_requires=install_requires,
     classifiers=[
         'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Intended Audience :: Science/Research',
